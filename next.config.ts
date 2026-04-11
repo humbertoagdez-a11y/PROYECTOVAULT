ae25498bcebae7930ecddc7dff50bbfb772eb5a1
humbertoagdez-a11y <humbertoagdez@gmail.com>
2026-04-11 00:12:07 -0300

    deploy: forcing build with ignore config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Esto obliga a Vercel a ignorar cualquier error de TypeScript y terminar el build
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignoramos también ESLint para evitar bloqueos por formato
     ignoreDuringBuilds: true,
   },
+  // Desactivamos indicadores de desarrollo que puedan molestar
+  devIndicators: {
+    buildActivity: false,
+  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
